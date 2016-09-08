0166bf1d32665d0f6001b3bbc8b468907fba2d59
k7696 <k7696@MEDI-426-WS-03>
2016-09-08 18:38:05 +0300

    Demo ykköset tehty

---
+++ Saitti/hae-tyontekijat-ilta.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="hae-tyontekijat-ilta.aspx.cs" Inherits="hae_tyontekijat_ilta" %>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Firman X työntekijät</title>
+    <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+</head>
+<body>
+    <!-- Sivulla pitää olla aina ko. form, jonka sisään tulee featuret. -->
+    <form id="form1" runat="server">
+        <div id="nappulat">
+            <asp:Button ID="btnHae" Class="w3-btn w3-green" runat="server" Text="Hae työntekijät" OnClick="btnHae_Click" />
+        </div>
+        <div id="esitys">
+            <asp:GridView ID="gvData" Class="w3-green" runat="server"></asp:GridView>
+        </div>
+        <div id="footer">
+            <asp:Label ID="lglMessage" Class="w3-container w3-teal" runat="server" Text="Label">...</asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Saitti/hae-tyontekijat-ilta.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Configuration; // Web.Configia varten
+using System.Data; // Ado.net-luokkia varten
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class hae_tyontekijat_ilta : System.Web.UI.Page
+{
+
+    string xmlFilu; 
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        // Luetaan Web.Config:sta xml-tiedoston nimi.
+        xmlFilu = ConfigurationManager.AppSettings["tiedosto"];
+        lglMessage.Text = xmlFilu;
+    }
+
+    protected void btnHae_Click(object sender, EventArgs e)
+    {
+        // Luetaan xml-tiedostosta työntekijöiden tiedot ja esitetään ne gridView:ssa.
+        DataSet ds = new DataSet();
+        DataTable dt = new DataTable();
+        DataView dv = new DataView(); // Filtteröintiä ja suoritusta varten
+
+        ds.ReadXml(Server.MapPath(xmlFilu)); // Kannattaa käyttää Server.MapPathia
+        dt = ds.Tables[0];
+        dv = dt.DefaultView;
+
+        // Datan sitominen ui-kontrolliin
+        gvData.DataSource = dv;
+        gvData.DataBind();
+
+        lglMessage.Text = string.Format("Löytyi {0} työntekijää", dt.Rows.Count);
+    }
+}
