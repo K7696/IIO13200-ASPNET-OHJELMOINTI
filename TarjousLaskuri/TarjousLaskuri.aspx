dd0519de28c7ce8e0e4e4373a7c3da4e277191d1
maikku <maikku@MAILIS>
2016-09-16 00:12:34 +0300

    Viikkotehtävä1 tehty

---
+++ TarjousLaskuri/TarjousLaskuri.aspx
@@ -0,0 +1,92 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="TarjousLaskuri.aspx.cs" Inherits="TarjousLaskuri" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link 
+        rel="stylesheet" 
+        href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" 
+        integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" 
+        crossorigin="anonymous">
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="col-md-12">
+            <h1>IKKUNAN TARJOUSLASKURI</h1>
+        </div>
+        <div class="col-md-3">
+            <div class="col-md-offset-1">
+                <div class="row form-group">
+                    <div class="">
+                        <asp:Label ID="lbl1" class="" runat="server" Text="Ikkunan Leveys (L):"></asp:Label>
+                    </div>
+                    <div class="">
+                        <asp:TextBox ID="tbWindowWidth" class="form-control input-sm" runat="server"></asp:TextBox>
+                    </div>       
+                </div>
+                <div class="row form-group">
+                    <div class="">
+                        <asp:Label ID="lbl2" class="" runat="server" Text="Ikkunan korkeus (H):"></asp:Label>
+                    </div>
+                    <div class="">
+                        <asp:TextBox ID="tbWindowHeight" class="form-control input-sm" runat="server"></asp:TextBox>
+                    </div>
+                </div>
+                <div class="row form-group">
+                    <div class="">
+                        <asp:Label ID="lbl3" class="" runat="server" Text="Karmipuun leveys (W):"></asp:Label>
+                    </div>
+                    <div class="">
+                        <asp:TextBox ID="tbFrameWidth" class="form-control input-sm" runat="server"></asp:TextBox>
+                    </div>
+                </div>
+                <div class="text-info"><b>Huom.</b> Syötä ikkunan ja karmin mitat millimetreinä.</div>
+                <br />
+                <div class="row form-group">
+                    <div>
+                        <asp:Button ID="btnCalculateOffer" class="btn btn-primary btn-lg" runat="server" Text="Laske tarjoushinta" OnClick="btnCalculateOffer_Click" />
+                    </div>
+                </div>
+                <div class="row form-group">
+                    <div class="">
+                        <asp:Label ID="lbl4" class="" runat="server" Text="Ikkunan pinta-ala:"></asp:Label>
+                    </div>
+                    <div class="bg-info">
+                        <asp:Label ID="lblWindowArea" class=""  runat="server" Text="..."></asp:Label>
+                    </div>
+                </div>
+                <div class="row form-group">
+                    <div class="">
+                        <asp:Label ID="lbl5" class="" runat="server" Text="Karmin piiri:"></asp:Label>
+                    </div>
+                    <div class="bg-info">
+                        <asp:Label ID="lblFrameCircuit" class=""  runat="server" Text="..."></asp:Label>
+                    </div>
+                </div>
+                <div class="row form-group">
+                    <div class="">
+                        <asp:Label ID="lbl6" class="" runat="server" Text="Tarjoushinta (ilman ALV)"></asp:Label>
+                    </div>
+                    <div class="bg-info">
+                        <asp:Label ID="lblPrice" class=""  runat="server" Text="..."></asp:Label>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-9">
+            <asp:Image ID="imgWindow" runat="server" />
+        </div>
+        <div class="row col-md-12">
+            <div class="col-md-offset-1">
+                <asp:Label ID="lblSystemMessage" class="text-warning" runat="server" Text=""></asp:Label>
+            </div>
+        </div>
+    </form>
+<script 
+    src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" 
+    integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" 
+    crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ TarjousLaskuri/TarjousLaskuri.aspx.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+
+public partial class TarjousLaskuri : System.Web.UI.Page
+{
+
+    #region Properties
+
+    /// <summary>
+    /// kate% = 30% 
+    /// </summary>
+    private double marginPercent;
+
+    /// <summary>
+    /// LasinNelioHinta=45€/m2
+    /// </summary>
+    private double windowSquareMeterPrice;
+
+    /// <summary>
+    /// AlumiiniKarminJuoksumetriHinta=100€/jm 
+    /// </summary>
+    private double aluminiumFrameMeterPrice;
+
+    /// <summary>
+    /// Työmenekki=150€/ikkuna
+    /// </summary>
+    private double workPrice;
+
+    #endregion // Properties
+
+    #region Private methods
+
+    /// <summary>
+    /// Laske ikkunan pinta-ala
+    /// </summary>
+    /// <param name="width">Ikkunan leveys</param>
+    /// <param name="height">Ikkunan korkeus</param>
+    /// <param name="frame">Karmin leveys</param>
+    /// <returns></returns>
+    private double calculateWindowArea(double width, double height, double frameWidth) {
+        // Tässä on vähän hämäävä speksi, että onko ikkunan pinta-alan oltava lasin ala vai ala karmeineen?????
+        // Jos karmeineen, niin silloin allaoleva kaava ei toimi oikein?
+        return ((height - (2 * frameWidth)) / 1000) * ((width - (2 * frameWidth)) / 1000);
+    }
+
+    /// <summary>
+    /// Laske karmin piiri
+    /// </summary>
+    /// <param name="width">ikkunan leveys</param>
+    /// <param name="height">ikkunan korkeus</param>
+    /// <returns></returns>
+    private double calculateFrame(double width, double height)
+    {
+        return 2 * ((width / 1000) + (height / 1000));
+    }
+
+    /// <summary>
+    /// Laske ikkunan hinta
+    /// </summary>
+    /// <param name="windowArea">Ikkunan pinta-ala</param>
+    /// <param name="frame">Karmipuun piiri</param>
+    /// <returns></returns>
+    private double calculateWindowPrice(double windowArea, double frame)
+    {
+        return (1 + (marginPercent / 100)) * ((windowArea * windowSquareMeterPrice) + (frame * aluminiumFrameMeterPrice) + (workPrice));
+    }
+
+    /// <summary>
+    /// Lue web.config
+    /// </summary>
+    private void readConfiguration()
+    {
+        // Luetaan tarvittavat parametrit web.config:sta
+        bool marginPercentParse = double.TryParse(
+                ConfigurationManager.AppSettings["marginPercent"], out marginPercent);
+
+        bool windowSquareMeterPriceParse = double.TryParse(
+            ConfigurationManager.AppSettings["windowSquareMeterPrice"], out windowSquareMeterPrice);
+
+        bool aluminiumFrameMeterPriceParse = double.TryParse(
+                ConfigurationManager.AppSettings["aluminiumFrameMeterPrice"], out aluminiumFrameMeterPrice);
+
+        bool workPriceParse = double.TryParse(
+                ConfigurationManager.AppSettings["workPrice"], out workPrice);
+
+
+        if (!marginPercentParse ||
+            !windowSquareMeterPriceParse ||
+            !aluminiumFrameMeterPriceParse ||
+            !workPriceParse)
+        {
+            // Ei saatu luettua web.config:sta, joten heitä poikkeus
+            throw new Exception("Varoitus: Ohjelman alustuksessa tapahtui virhe. Ota yhteyttä ylläpitoon.");
+        }
+    }
+
+    /// <summary>
+    /// Aseta labelit default-arvoihin
+    /// </summary>
+    private void resetLabels()
+    {
+        lblSystemMessage.Text = "";
+        lblFrameCircuit.Text = "...";
+        lblWindowArea.Text = "...";
+        lblPrice.Text = "...";
+    }
+
+    /// <summary>
+    /// Ikkunan lasku
+    /// </summary>
+    private void calculateWindowPrice()
+    {
+        // Tyhjennä edelliset arvot
+        resetLabels();
+
+        // Alusta tarvittavat muuttujat
+        double windowWidth = 0;
+        double windowHeight = 0;
+        double frameWidth = 0;
+
+        // Ota kentistä arvot
+        string strWindowWidth = tbWindowWidth.Text;
+        string strWindowHeight = tbWindowHeight.Text;
+        string strFrameWidth = tbFrameWidth.Text;
+
+        // Parse doubles
+        bool windowWidthParse = double.TryParse(strWindowWidth, out windowWidth);
+        bool windowHeightParse = double.TryParse(strWindowHeight, out windowHeight);
+        bool frameParse = double.TryParse(strFrameWidth, out frameWidth);
+
+        if (windowWidth == 0.0 ||
+            windowHeight == 0.0 ||
+            !windowWidthParse ||
+            !windowHeightParse ||
+            !frameParse)
+        {
+            // Heitä poikkeus virheellisistä syötteistä
+            throw new Exception("Varoitus: Ikkunan hintaa ei voitu laskea. Tarkasta syötteet.");
+        }
+        else
+        {
+            double frame = 0;
+            double windowArea = 0;
+            double windowPrice = 0;
+
+            // Laske ikkunan pinta-ala
+            windowArea = calculateWindowArea(windowWidth, windowHeight, frameWidth);
+            lblWindowArea.Text = windowArea.ToString();
+
+            // Laske karmin piiri
+            frame = calculateFrame(windowWidth, windowHeight);
+            lblFrameCircuit.Text = frame.ToString();
+
+            // Laske ikkunan hinta
+            windowPrice = calculateWindowPrice(windowArea, frame);
+            lblPrice.Text = Math.Round(windowPrice, 2).ToString();
+        }
+    }
+
+    #endregion // Private methods
+
+    #region Protected methods
+
+    /// <summary>
+    /// Suoritetaan sivun latautuessa
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        try { 
+            // Lue configuraatio
+            readConfiguration();
+        }
+        catch (Exception ex)
+        {
+            // Näytä poikkeus
+            lblSystemMessage.Text = ex.Message;
+        }
+    }
+
+    /// <summary>
+    /// Suoritetaan kun klikataan laske tarjoushintapainiketta
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    protected void btnCalculateOffer_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            // Laske ikkunan hinta
+            calculateWindowPrice();
+        }
+        catch (Exception ex)
+        {
+            // Näytä poikkeus
+            lblSystemMessage.Text = ex.Message;
+        }
+    }
+
+    #endregion // Protected methods
+}
