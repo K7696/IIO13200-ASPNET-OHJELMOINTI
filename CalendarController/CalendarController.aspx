9834033cc039338fad54799d3e33f1f363ff72b4
maikku <maikku@MAILIS>
2016-09-24 09:33:11 +0300

    Viikkotehtävä 3 tehty

---
+++ CalendarController/CalendarController.aspx
@@ -0,0 +1,49 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="CalendarController.aspx.cs" Inherits="CalendarController" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>CalendarController</title>
+    
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="">
+            <div>
+                <h1><asp:Label ID="lblToday" runat="server" Text=""></asp:Label></h1>
+            </div>
+            <div>
+                <span><b>Valittu päivämäärä:</b></span>
+                <asp:Label ID="lblChosenDate" runat="server" Text=""></asp:Label>
+                <br />
+                <span><b>Valitun päivän ja tämän päivän erotus: </b></span>
+                <asp:Label ID="lblAllDays" runat="server" Text=""></asp:Label>
+                <span> päivää</span>
+                <br />
+                <span><b>Päivää / Kuukautta / Vuotta</b></span>
+                <br />
+                <asp:Label ID="lblDateDifference" runat="server" Text=""></asp:Label>
+                <span> päivää /</span>
+                <asp:Label ID="lblMonthDifference" runat="server" Text=""></asp:Label>
+                <span> kuukautta /</span>
+                <asp:Label ID="lblYearDifference" runat="server" Text=""></asp:Label>
+                <span> vuotta</span>
+            </div>
+            <br />
+            <div>
+                <asp:Button ID="bntPrevious" runat="server" OnClick="bntPrevious_Click" Text="< Vuosi" />
+                <asp:Button ID="btnNext" runat="server" OnClick="btnNext_Click" Text="Vuosi >" />
+                <br />
+                <asp:Calendar ID="Calendar1" runat="server" OnSelectionChanged="Calendar1_SelectionChanged">
+                   
+                </asp:Calendar>
+            </div>
+        </div>
+        <br />
+        <div>
+            <asp:Label ID="lblSystemMessage" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ CalendarController/CalendarController.aspx.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DateTimeSpan;
+
+public partial class CalendarController : System.Web.UI.Page
+{
+    #region Properties
+
+    private DateTime _today { get; set; }
+    private DateTime _selectedDate { get; set; }
+    private string _selectedDateStr { get; set; }
+    private readonly string _cookieName = "Today";
+
+    // Poikkeudet
+    private readonly string _cookieError = "Varoitus: Keksistä lukeminen epäonnistui.";
+    private readonly string _error = "Varoitus: Päivien lukumäärän laskeminen epäonnistui.";
+
+    #endregion // Properties
+
+    #region Private methods
+
+    /// <summary>
+    /// Laske päivämääräerotus
+    /// </summary>
+    private void _calculateDateDifference()
+    {
+        // Kalenterin valittupäivä
+        _selectedDateStr = Calendar1.SelectedDate.ToShortDateString();
+        lblChosenDate.Text = _selectedDateStr;
+        _selectedDate = DateTime.Parse(_selectedDateStr);
+
+        // Laske päivien erotus (Voidaan laskea myös näin)
+        TimeSpan dateDifference = (DateTime.Now.Date - _selectedDate);
+        // Päivinä yht.
+        lblAllDays.Text = dateDifference.TotalDays.ToString();
+
+        // Helpompi tapa saada päivämäärien erot on
+        var dateSpan = DateTimeSpan.DateTimeSpan.CompareDates(_selectedDate, DateTime.Now.Date);
+
+        // Päivät
+        lblDateDifference.Text = dateSpan.Days.ToString();
+
+        // Kuukaudet
+        lblMonthDifference.Text = dateSpan.Months.ToString();
+
+        // Vuodet
+        lblYearDifference.Text = dateSpan.Years.ToString();
+    }
+
+    /// <summary>
+    /// Aseta kalenterin päivä sivulle tultaessa ekaa kertaa
+    /// </summary>
+    private void _setCalendarDateOnLoad()
+    {
+        Calendar1.VisibleDate = _today;
+        Calendar1.TodaysDate = _today;
+        Calendar1.SelectedDate = _today;
+    }
+
+    /// <summary>
+    /// Kirjoita _today keksiin
+    /// </summary>
+    private void _createCookie()
+    {
+        // Luodaan keksi ja kirjoitetaan siihen viesti
+        HttpCookie cookie = new HttpCookie(_cookieName, _today.ToString());
+        cookie.Expires = DateTime.Now.AddMinutes(30);
+        Response.Cookies.Add(cookie);
+    }
+
+    /// <summary>
+    /// Lue päivämäärä keksistä
+    /// </summary>
+    private void _readCookie()
+    {
+        // Tarkastetaan, että löytyykö keksiä
+        if(Request.Cookies[_cookieName] != null)
+        {
+            string strToday = Request.Cookies[_cookieName].Value;
+            DateTime dt;
+            bool todayParse = DateTime.TryParse(strToday, out dt);
+
+            if(todayParse)
+            {
+                _today = dt;
+            }
+            else
+            {
+                throw new Exception(_cookieError);
+            }
+        }
+        else
+        {
+            throw new Exception(_cookieError);
+        }
+
+    }
+
+    #endregion // Private methods
+
+    #region Protected methods
+
+    /// <summary>
+    /// Sivun latautuessa
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        // Tultiin sivulle, joten aseta tämä päivä
+        lblToday.Text = "TÄNÄÄN ON " + DateTime.Today.ToShortDateString();
+
+        if (IsPostBack)
+        {
+            try
+            {
+                // Luetaan keksiin tallennettu päivämäärä
+                _readCookie();
+            }
+            catch (Exception ex)
+            {
+                lblSystemMessage.Text = ex.Message;
+            }
+            
+        }
+        else
+        {
+            try
+            {
+                // Alustetaan nykyhetki
+                _today = DateTime.Today;
+
+                // Kirjoitetaan nykyhetki keksiin
+                _createCookie();
+
+                // Asetetaan kalenteri
+                _setCalendarDateOnLoad();
+            }
+            catch (Exception ex)
+            {
+                lblSystemMessage.Text = ex.Message;
+            }
+        }
+    }
+
+    
+
+    /// <summary>
+    /// Valitaan päivä kalenterista
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    protected void Calendar1_SelectionChanged(object sender, EventArgs e)
+    {
+        try
+        {
+            _calculateDateDifference();
+        }
+        catch (Exception)
+        {
+            lblSystemMessage.Text = _error;
+        }
+    }
+
+    /// <summary>
+    /// Vähennä vuosi
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    protected void bntPrevious_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            // Vähennä vuosi
+            _today = new DateTime(_today.Year, _today.Month, _today.Day).AddMonths(-12);
+            _setCalendarDateOnLoad();
+            _createCookie();
+        }
+        catch (Exception)
+        {
+            lblSystemMessage.Text = _error;
+        }
+    }
+
+    /// <summary>
+    /// Lisää vuosi
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    protected void btnNext_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            // Lisää vuosi
+            _today = new DateTime(_today.Year, _today.Month, _today.Day).AddMonths(12);
+            _setCalendarDateOnLoad();
+            _createCookie();
+        }
+        catch (Exception)
+        {
+            lblSystemMessage.Text = _error;
+        }
+    }
+
+    #endregion // Protected methods
+
+}
