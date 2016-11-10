fa8b69f011156f1799bae9a4adc4a3d82b50ba9b
maikku <maikku@MAILIS>
2016-11-11 00:19:53 +0200

    Viikkotehtävä 8 Finnkino

---
+++ FinnKino/Index.aspx
@@ -0,0 +1,47 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="Index.aspx.cs" Inherits="Index" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="header" Runat="Server">
+<div class="w3-margin-left">
+    <div class="w3-top w3-blue-grey">
+        <h1 class="w3-margin-left">FINNKINO</h1>
+    </div>
+    <div class="w3-sidenav w3-collapse w3-white w3-animate-left" style="margin-top:65px;width:270px">
+        <asp:ListBox ID="ListBox1" AutoPostBack="True" OnSelectedIndexChanged="ListBox1_SelectedIndexChanged" runat="server" style="width:250px;height:400px;overflow:scroll;"></asp:ListBox>
+    </div>
+    <div class="w3-main w3-container" style="margin-left:270px;margin-top:60px;">
+        <h3>Leffat <asp:Label ID="lblHeader" runat="server" Text=""></asp:Label> <asp:Label ID="date" runat="server" Text=""></asp:Label></h3>
+        <asp:Label ID="err" runat="server" Text=""></asp:Label>
+        <div>
+            <asp:Repeater ID="rptMovies" runat="server">
+              <HeaderTemplate>
+                <table>
+                  <thead>
+                    <tr>
+                        <th></th>
+                    </tr>
+                  </thead>
+                  <tbody>
+              </HeaderTemplate>
+              <ItemTemplate>
+                <tr>
+                  <td><img src="<%# Eval("RatingImageUrl") %>" alt="img" /></td>
+                </tr>
+              </ItemTemplate>
+              <FooterTemplate>
+                </tbody>
+                </table>
+              </FooterTemplate>
+            </asp:Repeater>
+        </div>
+    </div>
+</div>
+<br />
+
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="body" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content4" ContentPlaceHolderID="footer" Runat="Server">
+</asp:Content>
+
---
+++ FinnKino/Index.aspx.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+
+public class Theatre
+{
+    #region Properties
+
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    #endregion Properties
+}
+
+public class Movie
+{
+    #region Properties
+
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string RatingImageUrl { get; set; }
+
+    #endregion Properties
+}
+
+public partial class Index : System.Web.UI.Page
+{
+    #region Private methods
+
+    private List<Movie> getMovies(string id)
+    {
+        List<Movie> list = new List<Movie>();
+
+        try
+        {
+            XmlDocument doc = new XmlDocument();
+
+            var monday = DateTime.Today.AddDays(-(int)DateTime.Today.DayOfWeek + (int)DayOfWeek.Monday);
+
+            date.Text = string.Format("{0}-{1}", monday.ToShortDateString(), monday.AddDays(7).ToShortDateString());
+
+            // Load data  
+            doc.Load(string.Format("http://www.finnkino.fi/xml/Schedule/?area={0}&dt={1}&nrOfDays={2}", id, monday.ToShortDateString(), 7));
+
+            XmlNodeList nodes = doc.SelectNodes("//Shows/Show");
+
+            foreach (XmlNode node in nodes)
+            {
+                list.Add(new Movie
+                {
+                    Id = int.Parse(node.ChildNodes[0].InnerText),
+                    Name = node.ChildNodes[15].InnerText,
+                    RatingImageUrl = node.ChildNodes[35].ChildNodes[2].InnerText
+                });
+            }
+        }
+        catch (Exception)
+        {
+            err.Text = "Elokuvia ei voitu nyt hakea.";
+        }
+       
+        return list;
+    }
+
+    private List<Theatre> getTheatres()
+    {
+        List<Theatre> list = new List<Theatre>();
+
+        try
+        {
+            XmlDocument doc = new XmlDocument();
+
+            // Load data  
+            doc.Load("http://www.finnkino.fi/xml/TheatreAreas/");
+ 
+            XmlNodeList nodes = doc.SelectNodes("//TheatreArea");
+
+            foreach (XmlNode node in nodes)
+            {
+                list.Add(new Theatre
+                {
+                    Id = int.Parse(node.ChildNodes[0].InnerText),
+                    Name = node.ChildNodes[1].InnerText
+                });
+            }
+        }
+        catch (Exception)
+        {
+            err.Text = "Teattereiden haku ei onnistunut.";
+        }
+
+        return list;
+    }
+
+    #endregion Private methods
+
+    #region Protected methods
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            List<Theatre> list = getTheatres();
+
+            foreach (var item in list)
+            {
+                ListBox1.Items.Add(new ListItem(item.Name, item.Id.ToString()));
+            }
+        }
+    }
+
+    protected void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        // Get the currently selected item in the ListBox.
+        ListItem item = ListBox1.SelectedItem;
+        string id = item.Value;
+        string name = item.Text;
+
+        lblHeader.Text = name;
+
+        List<Movie> list = getMovies(id);
+
+        rptMovies.DataSource = list;
+        rptMovies.DataBind();
+    }
+
+    #endregion Protected methods
+
+}
